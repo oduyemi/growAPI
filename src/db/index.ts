564749e8ebc7c7d06f8c34824c766247ac7cde84
Yemi Oduyemi <yhermii@gmail.com>
2024-04-11 10:54:23 +0100

    Integrate MongoDB for session storage

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,17 @@
 import mongoose, { Connection } from "mongoose";
+import session from 'express-session';
+import MongoDBSessionStore from 'connect-mongodb-session';
 require('dotenv').config();
 
-
-const mongoDB: string = process.env.MONGODB_URI !== undefined ? process.env.MONGODB_URI : "mongodb://127.0.0.1:27017/growAfricadb";
-
-
+const mongoDBURI: string = process.env.MONGODB_URI !== undefined ? process.env.MONGODB_URI : "mongodb://127.0.0.1:27017/growAfricadb";
 
 mongoose
-  .connect(mongoDB)
+  .connect(mongoDBURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .catch((e: Error) => {
-    console.error("connection-error", e.message);
+    console.error("MongoDB connection error:", e.message);
   });
 
 const db: Connection = mongoose.connection;
@@ -22,4 +24,15 @@ db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
-export default db;
+// Setup MongoDBSessionStore
+const MongoDBStore = MongoDBSessionStore(session);
+const store = new MongoDBStore({
+  uri: mongoDBURI,
+  collection: 'sessions' 
+});
+
+store.on('error', function(error) {
+  console.error('Session Store Error:', error);
+});
+
+export { db, store };
